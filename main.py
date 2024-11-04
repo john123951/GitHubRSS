77a4623db3df8443e1395b180095734524a47b33
sweet <sweet@apple.com>
2024-11-04 17:47:45 +0800

    feat: update workflows

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import asyncio
+import json
+import os
 from datetime import datetime
 
 from trending.crawler import github_trending_crawler
@@ -14,13 +16,23 @@ def trending_daily_scheduler():
 
     # params
     current_date = datetime.now()
-    formatted_date = current_date.strftime('%Y%m%d')
+    year = current_date.strftime('%Y')
+    month = current_date.strftime('%m')
+    today = current_date.strftime('%Y%m%d')
+
+    # create folder
+    save_folder = os.path.join('.', 'data', year, month)
+    os.makedirs(os.path.dirname(save_folder), exist_ok=True)
 
     # save rss
-    save_path = f'./data/{formatted_date}-trending.xml'
-    with open(save_path, 'w+', encoding='utf-8') as file:
+    rss_save_path = os.path.join(save_folder, f'{today}-trending.xml')
+    with open(rss_save_path, 'w', encoding='utf-8') as file:
         file.write(trending_rss)
-        file.flush()
+
+    # save json
+    json_save_path = os.path.join(save_folder, f'{today}-trending.json')
+    with open(json_save_path, 'w', encoding='utf-8') as file:
+        file.write(json.dumps(trending_list, ensure_ascii=False))
 
     pass
 
